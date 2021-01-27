f30caa8e880a36f710f3426387fb961069e4f979
AyaDesigner <aya.berdyeva@gmail.com>
2021-01-27 15:36:44 +0100

    added tests

--- capitalizeFirstLetter.js
+++ capitalizeFirstLetter.js
@@ -3,9 +3,14 @@ const assert = require('assert');
 
 // WRITE THE ACTUAL FUNCTION HERE
 function capitalizeFirst(input) {
-    return input.length > 0
-      ? input[0].toUpperCase() + input.slice(1)
-      : '';
+    
+    const newStr =  input.split(" ");
+    let output = [];
+  
+    for (let i = 0; i<newStr.length; i++){
+      output.push(newStr[i].charAt(0).toUpperCase() + newStr[i].substring(1));
+    }
+    return output.join(" ");
   }
   
 
@@ -21,5 +26,15 @@ assert.strictEqual(capitalizeFirst('javaScript'), 'JavaScript');
 // Check that it works for a 1-character string
 assert.strictEqual(capitalizeFirst('z'), 'Z');
 
-// Check that it works for an empty string
+// Check that it works for string with one word
+assert.strictEqual(capitalizeFirst('word'), 'Word');
+
+// Check that it works for string with several words
+assert.strictEqual(capitalizeFirst('I am learning'), 'I Am Learning');
+
+// Check that it works for empty string 
 assert.strictEqual(capitalizeFirst(''), '');
+
+
+
+console.log(capitalizeFirst("i am learning TDD"));
